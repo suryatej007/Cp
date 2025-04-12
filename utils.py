ef93d3b893079598e89402abaf8e330d7a2f6948
DASARI SIVA SAI SURYA TEJA <110438431+suryatej007@users.noreply.github.com>
2025-04-12 10:30:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,19 +6,21 @@ scaler = joblib.load("scaler.pkl")
 rf_model = joblib.load("random_forest.pkl")
 autoencoder = load_model("autoencoder.h5", compile=False)
 
-THRESHOLD = 0.01  # tune this based on validation loss
+THRESHOLD = 0.02  # tune this based on validation loss
 
 def preprocess_input(user_input):
-    data = np.array(user_input).reshape(1, -1)
-    scaled = scaler.transform(data)
-    return scaled
+    arr = np.array(user_input).reshape(1, -1)
+    return scaler.transform(arr)
+
+
 
 def predict_combined(scaled_input):
     rf_pred = rf_model.predict(scaled_input)[0]
-    reconstructed = autoencoder.predict(scaled_input)
-    ae_error = np.mean(np.square(scaled_input - reconstructed))
+    recon = autoencoder.predict(scaled_input)
+    error = np.mean(np.square(scaled_input - recon))
 
-    if rf_pred == 1 or ae_error > THRESHOLD:
+    if rf_pred == 1 or error > THRESHOLD:
         return "Attack Detected"
     else:
         return "Normal Traffic"
+
