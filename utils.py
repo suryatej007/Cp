e0ab81ad0580ffce391eceaf0d0befac0503d387
DASARI SIVA SAI SURYA TEJA <110438431+suryatej007@users.noreply.github.com>
2025-04-12 10:56:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def predict_combined(scaled_input):
     rf_pred = rf_model.predict(scaled_input)[0]
     recon = autoencoder.predict(scaled_input)
     error = np.mean(np.square(scaled_input - recon))
-    if recon_error > threshold or rf_prediction == 1:
+    if recon > threshold or rf_pred == 1:
         return "Attack Detected"
     else:
         return "Normal Traffic"
