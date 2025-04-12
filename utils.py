6e1feb1a370a5d70a9d69560efe73d153c470311
DASARI SIVA SAI SURYA TEJA <110438431+suryatej007@users.noreply.github.com>
2025-04-12 10:34:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,13 @@ from tensorflow.keras.models import load_model
 scaler = joblib.load("scaler.pkl")
 rf_model = joblib.load("random_forest.pkl")
 autoencoder = load_model("autoencoder.h5", compile=False)
+threshold = joblib.load("autoencoder_threshold.pkl")
 
-THRESHOLD = 0.02  # tune this based on validation loss
+recon = autoencoder.predict(X_scaled)
+errors = np.mean(np.square(X_scaled - recon), axis=1)
+threshold = np.percentile(errors, 95)
+joblib.dump(threshold, "autoencoder_threshold.pkl")
+
 
 def preprocess_input(user_input):
     arr = np.array(user_input).reshape(1, -1)
@@ -18,8 +23,10 @@ def predict_combined(scaled_input):
     rf_pred = rf_model.predict(scaled_input)[0]
     recon = autoencoder.predict(scaled_input)
     error = np.mean(np.square(scaled_input - recon))
+    print("🟡 RF Prediction:", rf_pred)
+    print("🟡 AE Error:", error, "| Threshold:", threshold)
 
-    if rf_pred == 1 or error > THRESHOLD:
+    if rf_pred == 1 or error > threshold:
         return "Attack Detected"
     else:
         return "Normal Traffic"
